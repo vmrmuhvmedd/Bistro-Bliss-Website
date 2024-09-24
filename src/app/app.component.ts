a6cc26eca10bbb8d31aa86223099e60ac2a142b7
Amr Muhamedd <115637842+vmrmuhvmedd@users.noreply.github.com>
2024-09-24 23:36:11 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { BookTableComponent } from './book-table/book-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderNavComponent, HomeComponent, FooterComponent, MenuComponent, BookTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/header-nav/header-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-nav.component.html',
+  styleUrl: './header-nav.component.css'
+})
+export class HeaderNavComponent {
+
+}
